f106996afdceb54da3248e7278765d4a457886ca
MACARENA FILLOL <mafillol@uc.cl>
2019-04-27 17:01:23 -0400

    add info trabajadores file parametros.py

--- parametros.py
+++ parametros.py
@@ -5,7 +5,11 @@ CB = 475
 W = {"skin": 1, "troqueladora": 2, "selladora": 2}
 
 # Trabajadores
-t = {"chery": {"sueldo": 11784, "mujer": 1}, "fontus": {"sueldo": 11673,
-    "mujer": 0}, "marcelin": {"sueldo": 11674, "mujer": 0}, "miranda": {
-    "sueldo": 15000, "mujer": 1}, "pierre": {"sueldo": 11822, "mujer": 0},
-    "philomene": {"sueldo": }}
+t = {"chery": {"sueldo": 11784, "mujer": 1, "chileno": 0},
+     "fontus": {"sueldo": 11673, "mujer": 0, "chileno": 0},
+     "marcelin": {"sueldo": 11674, "mujer": 0, "chileno": 1},
+     "miranda": {"sueldo": 15000, "mujer": 1, "chileno": 1},
+     "pierre": {"sueldo": 11822, "mujer": 0, "chileno": 0},
+     "philomene": {"sueldo": 11595, "mujer": 1, "chileno": 0},
+     "patricia": {"sueldo": 9600, "mujer": 1, "chileno": 1},
+     "bernarda": {"sueldo": 15000, "mujer": 1, "chileno": 1}}
