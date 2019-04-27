b64394a39a4e936aa3774d0fbb2dfbd92f196c7d
MACARENA FILLOL <mafillol@uc.cl>
2019-04-27 17:36:06 -0400

    add productos, proveedores, trabajadoes, dias, maquinas, clientes, produccion to conjuntos.py

---
+++ conjuntos.py
@@ -0,0 +1,29 @@
+# Productos
+I = ["skin", "blister", "electronico", "regulador"]
+
+# Recursos
+J = ["rigido 200", "rigido 150", "flexibe 150"]
+
+# Trabajadores
+K = ["chery", "fontus", "marcelin", "miranda", "pierre", "philomene",
+     "patricia", "bernarda", "joselin"]
+
+# Proveedores
+P = ["pv1", "pv2"]
+
+# Maquinas
+M = ["skin", "troqueladora", "selladora"]
+
+# Dias
+D = [i for i in range(1, 21)]
+
+# Clientes
+C = ["imp", "sfr", "viso", "ball"]
+
+# Etapas de produccion
+L = {"blister": ("skin", "troqueladora", "selladora",
+                 "relleno", "etiquetado", "envasado"),
+     "skin": ("skin", "toqueladora", "etiquetado", "envasado"),
+     "electronico": ("skin", "selladora", "desgajado", "etiquetado",
+                     "envasado")
+     "regulador": ("skin", "selladora", "envasado")}
