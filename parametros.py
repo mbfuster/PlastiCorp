61e95538c0dae455f542d994b44022a8219032bd
MACARENA FILLOL <mafillol@uc.cl>
2019-04-27 16:53:01 -0400

    add CB and W to parametros.py

---
+++ parametros.py
@@ -0,0 +1,11 @@
+# Capacidad de la bodega
+CB = 475
+
+# Trabajadores por máquina
+W = {"skin": 1, "troqueladora": 2, "selladora": 2}
+
+# Trabajadores
+t = {"chery": {"sueldo": 11784, "mujer": 1}, "fontus": {"sueldo": 11673,
+    "mujer": 0}, "marcelin": {"sueldo": 11674, "mujer": 0}, "miranda": {
+    "sueldo": 15000, "mujer": 1}, "pierre": {"sueldo": 11822, "mujer": 0},
+    "philomene": {"sueldo": }}
