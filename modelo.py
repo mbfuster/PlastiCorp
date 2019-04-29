6226639704e9ab2a7c57cc1a2574359dff4188c2
MACARENA FILLOL <mafillol@uc.cl>
2019-04-29 11:50:36 -0400

    generacion modelo in modelo.py

--- modelo.py
+++ modelo.py
@@ -1,1 +1,7 @@
 from gurobipy import Model, GRB
+from conjuntos.py import * # Todos los conjuntos de la modelacio
+from parametros.py import *  # Todos los parametros de la modelacio
+
+
+# Generacion de modelo
+model = Model("Factory Planning Plasticorp")
